8b6235c55b5fefcd42e780bfdffbcb322dc8cb0e
alexyap1205 <alexyap1205@live.com>
2018-07-26 21:02:45 +1000

    Add Ingredient

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.css';
 
 const layout = (props) => {
   return (
       <Auxillary>
           <div>Toolbar, Side Drawer, Backdrop</div>
-          <main>
+          <main className={classes.Content}>
               {props.children}
           </main>
       </Auxillary>
