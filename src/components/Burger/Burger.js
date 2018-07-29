53bf90236965216dd23f1c47298a01fdc4ff73b7
alexyap1205 <alexyap1205@live.com>
2018-07-29 20:20:55 +1000

    Add Ingredients Implementation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,22 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-    const transformedIngreadients = Object.keys(props.ingredients)
+    let transformedIngreadients = Object.keys(props.ingredients)
         .map(igKey => {
            return [...Array(props.ingredients[igKey])] // This returns a single array spreading all the content
                .map((_, i) => {
                return <BurgerIngredient key={igKey + i} type={igKey}/> // This defines the content of the top map
            })
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
 
+    if (transformedIngreadients.length === 0){
+        transformedIngreadients = <p>Please start adding ingreditents!</p>
+    }
+
+    console.log(transformedIngreadients);
   return (
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top"/>
